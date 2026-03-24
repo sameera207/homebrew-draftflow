a744cccc676670e5862825407286dbbf01a96905
sameera <sameera207@gmail.com>
2026-03-24 23:59:28 +1100

    fix: update SHA256 for v0.1.0 DMG

--- Casks/draftflow.rb
+++ Casks/draftflow.rb
@@ -1,6 +1,6 @@
 cask "draftflow" do
   version "0.1.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "3064b0687d7e7077e37d64da8575d110bfc6ab6110e86ea725ebb1306c6a1ce3"
 
   url "https://github.com/sameera207/draftflow/releases/download/v#{version}/Draftflow-#{version}.dmg"
   name "Draftflow"

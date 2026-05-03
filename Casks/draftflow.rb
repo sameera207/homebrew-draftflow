a02a8854c4ae4c2c0026ff13eafed40cc254fbfd
Sameera (Sam) <sameera207@gmail.com>
2026-05-03 15:58:22 +1000

    chore: update draftflow to v0.1.4

--- Casks/draftflow.rb
+++ Casks/draftflow.rb
@@ -1,6 +1,6 @@
 cask "draftflow" do
-  version "0.1.3"
-  sha256 "70c118d13590f58e059935bfb253ef39e909127b9e179bdf9097f429d5739964"
+  version "0.1.4"
+  sha256 "cc00ba71421d23ffb2023e948cbe8ef2a2a3e48e962b722b86f503d7acfc998e"
 
   url "https://github.com/sameera207/draftflow/releases/download/v#{version}/Draftflow-#{version}.dmg"
   name "Draftflow"
